d986989a25128d56a443e6f4c72aefbfd6b1aed1
Ahmed Elsayed Taha <120655825+AhmedBinAltaha@users.noreply.github.com>
2026-04-28 18:00:23 +0300

    Delete sql Queries directory

--- sql Queries/Database Exploration.sql
+++
@@ -1,9 +0,0 @@
--- Explore All Objects in The Database
-select * from INFORMATION_SCHEMA.TABLES; 
-
--- Eplore All Columns in The Database
-select * from INFORMATION_SCHEMA.COLUMNS;
-
-------
-select * from INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'dim_customers';
